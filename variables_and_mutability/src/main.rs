22b4d1f007f9b26e112f0c3236cc4f059759e036
xpertdreamer <yungnomen@gmail.com>
2026-01-02 14:38:05 +0300

    Complete Chapter 3.1

--- variables_and_mutability/src/main.rs
+++ variables_and_mutability/src/main.rs
@@ -1,3 +1,22 @@
 fn main() {
-    println!("Hello, world!");
+    // const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 *3;
+    //
+    // let mut x = 5;
+    // println!("The value of x is {x}");
+    // x+=1;
+    // println!("The value of x is {x}");
+    //
+    // println!("Const value: {THREE_HOURS_IN_SECONDS}");
+
+    // let x = 7;
+    // let x = x + 1;
+    // {
+    //     let x = x * 2;
+    //     println!("The value of x in tne inner scope is {x}");
+    // }
+    // println!("The value of x is {x}");
+
+    let spaces = "    ";
+    let spaces = spaces.len();
+    println!("The value of spaces is: {spaces}");
 }
